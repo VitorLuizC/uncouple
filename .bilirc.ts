cdd9b2c8d65bcd1aa48b41445bcbdb7e68050762
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2019-03-23 03:57:51 -0300

    :wrench: use Bili (rollup-plugin-typescript2) to transpile TS

---
+++ .bilirc.ts
@@ -0,0 +1,28 @@
+import { name } from './package.json';
+import { Config as Configuration } from 'bili';
+
+const configuration: Configuration = {
+  banner: true,
+  input: 'src/index.ts',
+  output: {
+    format: [
+      'es',
+      'cjs',
+      'umd',
+      'umd-min'
+    ],
+    moduleName: name
+  },
+  plugins: {
+    'typescript2': {
+      clean: true,
+      tsconfig: 'tsconfig.bundle.json',
+      useTsconfigDeclarationDir: true
+    }
+  },
+  babel: {
+    minimal: true
+  }
+};
+
+export default configuration;
