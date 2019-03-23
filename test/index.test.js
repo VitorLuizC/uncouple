157c5fd16606cf9fa7aeb02e41cd478a0d6146cf
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2019-03-22 23:33:55 -0300

    :white_check_mark: improve unit tests

--- test/index.test.js
+++ test/index.test.js
@@ -5,9 +5,27 @@ test('Module exports a uncouple function', (context) => {
   context.is(typeof uncouple, 'function');
 });
 
-test('Uncouple prototype methods', (context) => {
+test('Uncouple prototype methods from native constructors', (context) => {
   const { filter } = uncouple(Array);
   const names = ['Vitor', 'Lucas'];
 
   context.deepEqual(filter(names, (name) => name === 'Vitor'), ['Vitor']);
+});
+
+test('Uncouple prototype methods from classes', (context) => {
+  class Name {
+    constructor (...names) {
+      this.names = names;
+    }
+
+    join () {
+      return this.names.join(' ');
+    }
+  }
+
+  const { join } = uncouple(Name);
+
+  const name = new Name('Vitor', 'Luiz', 'Cavalcanti');
+
+  context.deepEqual(join(name), 'Vitor Luiz Cavalcanti');
 });
