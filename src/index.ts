40a19a353c1801c67577e979659fc164a6de1995
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2019-03-25 18:01:28 -0300

    :sparkles: create uncoupleMethodsAsCurries

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,31 @@
 type PropertyNameOf<T> = Extract<keyof T, string>;
 
 /**
+ * A generic function type.
+ */
+type Method = (...args: any[]) => any;
+
+/**
+ * Method name from `T`.
+ */
+type MethodNameOf<T> = keyof {
+  [K in PropertyNameOf<T>]: T[K] extends Method ? K : never
+};
+
+/**
+ * Curry to check if name is a method name from object, `T`.
+ * @param object
+ */
+const isMethodOf = <T>(object: T) => (
+  name: PropertyNameOf<T>
+): name is MethodNameOf<T> =>
+  name !== "constructor" && typeof object[name] === "function";
+
+/**
  * Uncoupled methods from `T`.
  */
 type UncoupledMethodsOf<T> = {
-  [K in PropertyNameOf<T>]: T[K] extends (...args: any[]) => any
+  [K in PropertyNameOf<T>]: T[K] extends Method
     ? (instance: T, ...args: Parameters<T[K]>) => ReturnType<T[K]>
     : never
 };
@@ -15,7 +36,7 @@ type UncoupledMethodsOf<T> = {
 /**
  * Constructor (class) with generic prototype `T`.
  */
-type Constructor<T> = (new (...args: any[]) => any) & { prototype: T };
+type Constructor<T> = { prototype: T };
 
 /**
  * Uncouple methods from constructor (class) into functions.
@@ -26,14 +47,46 @@ type Constructor<T> = (new (...args: any[]) => any) & { prototype: T };
  * ```
  * @param constructor - A constructor (class) to be uncoupled into functions.
  */
-const uncouple = <T>({ prototype }: Constructor<T>): UncoupledMethodsOf<T> => {
+const uncoupleMethods = <T>({
+  prototype
+}: Constructor<T>): UncoupledMethodsOf<T> => {
   const names = Object.getOwnPropertyNames(prototype) as PropertyNameOf<T>[];
-  return names.reduce((methods, name) => {
-    if (typeof prototype[name] === "function" && name !== "constructor")
-      // @ts-ignore
-      methods[name] = Function.call.bind(prototype[name]);
-    return methods;
-  }, Object.create(null) as UncoupledMethodsOf<T>);
+  const methods = Object.create(null) as UncoupledMethodsOf<T>;
+  names.filter(isMethodOf(prototype)).forEach(name => {
+    // @ts-ignore
+    methods[name] = Function.call.bind(prototype[name]);
+  });
+  return methods;
 };
 
-export default uncouple;
+/**
+ * Uncoupled methods from `T`.
+ */
+type UncoupledMethodsAsCurriesOf<T> = {
+  [K in PropertyNameOf<T>]: T[K] extends Method
+    ? (...args: Parameters<T[K]>) => (instance: T) => ReturnType<T[K]>
+    : never
+};
+
+/**
+ * Uncouple methods from constructor (class) into functions.
+ * @example ```js
+ * const { filter } = uncouple(Array);
+ * filter([ 1, 2, 3, 4 ], (value) => value % 2 === 0);
+ * //=> [ 2, 4 ]
+ * ```
+ * @param constructor - A constructor (class) to be uncoupled into functions.
+ */
+const uncoupleMethodsAsCurries = <T>({
+  prototype
+}: Constructor<T>): UncoupledMethodsAsCurriesOf<T> => {
+  const methods = Object.create(null) as UncoupledMethodsAsCurriesOf<T>;
+  const names = Object.getOwnPropertyNames(prototype) as PropertyNameOf<T>[];
+  names.filter(isMethodOf(prototype)).forEach(name => {
+    // @ts-ignore
+    methods[name] = (...args) => instance =>
+      // @ts-ignore
+      prototype[name].apply(instance, args);
+  });
+  return methods;
+};
